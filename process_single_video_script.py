12121ebee5d18ae29d68b3c12b088e1fefdedab9
Flavien SELLET <flavien.sellet@epitech.eu>
2020-12-07 15:50:18 +0100

    Should've been .xlsx

--- process_single_video_script.py
+++ process_single_video_script.py
@@ -2,9 +2,9 @@ from api.video_joints_positions import ProcessingVideoArgs
 from api.video_joints_positions import process_video
 
 
-source = "C:\\Users\\STIMO2-Admin\\data\\DSCF1240_stim-off.mp4"
-video_output = "C:\\Users\\STIMO2-Admin\\data\\DSCF1240_stim-off_skeleton.mp4"
-xls_output = "C:\\Users\\STIMO2-Admin\\data\\DSCF1240_stim-off_skeleton.xls"
+source = "C:\\Users\\STIMO2-Admin\\data\\DSCF1243_stim-on.mp4"
+video_output = "C:\\Users\\STIMO2-Admin\\data\\DSCF1243_stim-on_skeleton.mp4"
+xls_output = "C:\\Users\\STIMO2-Admin\\data\\DSCF1243_stim-on_skeleton.xlsx"
 
 extra_args = ProcessingVideoArgs()
 process_video(source, video_output, xls_output, extra_args)
